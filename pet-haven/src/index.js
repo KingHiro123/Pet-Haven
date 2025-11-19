c07e76b5f41d29df2802974b710a68aacc512af4
KingHiro123 <35801282+KingHiro123@users.noreply.github.com>
2025-11-19 19:38:28 +0800

    changes to index.js file

--- pet-haven/src/index.js
+++ pet-haven/src/index.js
@@ -7,7 +7,7 @@ import "./styles/global.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Pet-Haven">
       <App />
     </BrowserRouter>
   </React.StrictMode>
